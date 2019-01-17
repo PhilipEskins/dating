640fe994e1bc64959075e051ebc2ef2792e727e3
Drew Yoxsimer and Philip Eskins <drew+philip@>
2019-01-17 15:22:34 -0800

    add css style and fixed questionaire logic.

--- js/scripts.js
+++ js/scripts.js
@@ -6,34 +6,25 @@ $(document).ready(function() {
     var food = $("input:radio[name=food]:checked").val();
     var place = $("input:radio[name=place]:checked").val();
 
-    // if (hobby === "golf" && food === "soup" && place === "clubhouse") {
-    //   $("#caddyshack").show();
-    // }
-    // else if (hobby === "diving" && food === "cafeteriafood" && place === "school") {
-    //   $("#backtoschool").show();
-    // }
-    // else if (hobby === "running" && food === "bones" && place === "doghouse") {
-    //   $("#roverdan").show();
-    // }
-  if ( hobby === "golf" && food === "soup" || food === "soup" && place === "clubhouse") {
+    if ( hobby === "golf" && food === "soup" || food === "soup" && place === "clubhouse" || hobby === "golf" && place === "clubhouse") {
       $("#caddyshack").show();
       $("#roverdan").hide();
       $("#larryBurns").hide();
       $("#backtoschool").hide();
     }
-    else if ( hobby === "diving" && food === "cafeteriafood" || food === "cafeteriafood" && place === "school") {
+    else if ( hobby === "diving" && food === "cafeteriafood" || food === "cafeteriafood" && place === "school" || hobby === "diving" && place === "school") {
       $("#backtoschool").show();
       $("#roverdan").hide();
       $("#larryBurns").hide();
       $("#caddyshack").hide();
     }
-    else if ( hobby === "running" && food === "bones" || food === "bones" && place === "doghouse") {
+    else if ( hobby === "running" && food === "bones" || food === "bones" && place === "doghouse" || hobby === "running" && place === "doghouse") {
       $("#roverdan").show();
       $("#caddyshack").hide();
       $("#larryBurns").hide();
       $("#backtoschool").hide();
     }
-    else if (hobby === "other" && food === "other" && place === "other"){
+    else if (hobby === "other" || food === "other" || place === "other"){
     $("#larryBurns").show();
     $("#roverdan").hide();
     $("#caddyshack").hide();
