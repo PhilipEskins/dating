be39baf4dee9d8778ff986374a14790c9b3e7201
Drew Yoxsimer and Philip Eskins <drew@>
2019-01-17 14:50:12 -0800

    add it all. Late commit, sorry

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $("#questions").submit(function(event) {
+    event.preventDefault();
+
+    var hobby = $("input:radio[name=hobby]:checked").val();
+    var food = $("input:radio[name=food]:checked").val();
+    var place = $("input:radio[name=place]:checked").val();
+
+    // if (hobby === "golf" && food === "soup" && place === "clubhouse") {
+    //   $("#caddyshack").show();
+    // }
+    // else if (hobby === "diving" && food === "cafeteriafood" && place === "school") {
+    //   $("#backtoschool").show();
+    // }
+    // else if (hobby === "running" && food === "bones" && place === "doghouse") {
+    //   $("#roverdan").show();
+    // }
+  if ( hobby === "golf" && food === "soup" || food === "soup" && place === "clubhouse") {
+      $("#caddyshack").show();
+      $("#roverdan").hide();
+      $("#larryBurns").hide();
+      $("#backtoschool").hide();
+    }
+    else if ( hobby === "diving" && food === "cafeteriafood" || food === "cafeteriafood" && place === "school") {
+      $("#backtoschool").show();
+      $("#roverdan").hide();
+      $("#larryBurns").hide();
+      $("#caddyshack").hide();
+    }
+    else if ( hobby === "running" && food === "bones" || food === "bones" && place === "doghouse") {
+      $("#roverdan").show();
+      $("#caddyshack").hide();
+      $("#larryBurns").hide();
+      $("#backtoschool").hide();
+    }
+    else if (hobby === "other" && food === "other" && place === "other"){
+    $("#larryBurns").show();
+    $("#roverdan").hide();
+    $("#caddyshack").hide();
+    $("#backtoschool").hide();
+  }
+  });
+});
